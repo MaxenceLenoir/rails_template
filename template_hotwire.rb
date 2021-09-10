15e1fbf2c02c3acd99c50bcfbb9965e8b2269fe4
MaxenceLenoir <60671940+MaxenceLenoir@users.noreply.github.com>
2021-09-10 12:22:17 -0300

    Update template_hotwire.rb

--- template_hotwire.rb
+++ template_hotwire.rb
@@ -130,5 +130,5 @@ after_bundle do
   # Git
   ########################################
   git add: '.'
-  git commit: "-m 'Initial commit with MaxenceLenoir template"
+  git commit: "-m 'Initial commit with MaxenceLenoir template'"
 end
